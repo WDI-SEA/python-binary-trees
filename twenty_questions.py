9cee916cde852d694e6b7efb84afa1c6a26b116d
Steve Geluso <stevegeluso@gmail.com>
2017-02-17 12:10:19 -0800

    adding play while loop

--- twenty_questions.py
+++ twenty_questions.py
@@ -10,12 +10,12 @@ class QuestionTree:
         
     def play_game(self):
         print("Welcome to 20 Questions.")
-        self.play_game_helper(self.root)
+        self.root = self.play_game_helper(self.root)
         
     def play_game_helper(self, node):
         if node.left == None and node.right == None:
             print("You're thinking of:", node.data)
-            response = input("y/n?")
+            response = input("y/n? ")
             if response == "y":
                 print("I win!!")
                 return node
@@ -24,7 +24,7 @@ class QuestionTree:
                 return self.add_answer(node)
         else:
             print(node.data)
-            response = input("y/n?")
+            response = input("y/n? ")
             if response == "y":
                 node.right = self.play_game_helper(node.right)
             elif response == "n":
@@ -36,14 +36,34 @@ class QuestionTree:
           
     # create a new node that adds a dis-ambiguiating question
     # and a new answer.
-    def add_answer(self, node):
-        print("TODO: implement a method that returns a node containing")
-        print("TODO: a new question and answers on the bottom")
-        question = input("enter your disambiguating question")
-        yes_or_no = input("enter y/n for your question")
-        # TODO: build up the node on the left or the right.
-        new_node = QuestionNode("new node")
-        return new_node
+    def add_answer(self, old_node):
+        your_thing = input("enter the thing you were thinking of: ")
+        question = input("enter your disambiguating question: ")
+        yes_or_no = input("enter y/n for your thing: ")
+        
+        your_thing = QuestionNode(your_thing)
+        new_question = QuestionNode(question)
+        if yes_or_no == "y":
+            new_question.right = your_thing
+            new_question.left = old_node
+        elif yes_or_no == "n":
+            new_question.right = old_node
+            new_question.left = your_thing
+        else:
+            print("Invalid y/n input. Try again.")
+            return self.add_answer(old_node)
+        return new_question
         
 twenty_questions = QuestionTree()
-twenty_questions.play_game()    
+twenty_questions.play_game()
+
+keep_playing = True
+while keep_playing:
+    response = input("do you want to play again? ")
+    if response == "y":
+      twenty_questions.play_game()
+    elif response == "n":
+      print("Thanks for playing! Goodbye.")
+      keep_playing = False
+    else:
+        print("I'm sorry I didn't understand you. enter y/n")
